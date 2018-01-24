0edd7944214b814c9dab7824bf5f59b582f52078
Hugormen <hugo.ekelund@telia.com>
2018-01-24 22:54:52 +0100

    Add almost all tables, except the last 4

--- tables.sql
+++ tables.sql
@@ -25,3 +25,75 @@ login TEXT UNIQUE,
 FOREIGN KEY (program) REFERENCES Program(name),
 UNIQUE (ssn,program)
 );
+
+CREATE TABLE Branch(
+name TEXT,
+program TEXT,
+FOREIGN KEY (program) REFERENCES Program(name),
+PRIMARY KEY(name,program)
+);
+
+CREATE TABLE BelongsTo(
+student TEXT,
+branch TEXT,
+program TEXT,
+FOREIGN KEY (student) REFERENCES Student(ssn),
+FOREIGN KEY (student,program) REFERENCES Student(ssn,program),
+FOREIGN KEY (branch,program) REFERENCES Branch(name, program)
+);
+
+CREATE TABLE Course(
+code TEXT PRIMARY KEY,
+name TEXT,
+credits TEXT,
+department TEXT,
+FOREIGN KEY (department) REFERENCES Department(name)
+);
+
+CREATE TABLE Prerequisite(
+course TEXT,
+prerequisite TEXT,
+FOREIGN KEY (course) REFERENCES Course(code),
+FOREIGN KEY (prerequisite) REFERENCES Course(code),
+PRIMARY KEY (course,prerequisite)
+);
+
+CREATE TABLE Classification(
+name TEXT PRIMARY KEY
+);
+
+CREATE TABLE Classified(
+course TEXT,
+classification TEXT,
+FOREIGN KEY (course) REFERENCES Course(code),
+FOREIGN KEY (classification) REFERENCES Classification(name),
+PRIMARY KEY (classification, course)
+);
+
+CREATE TABLE MandatoryProgram(
+course TEXT,
+program TEXT,
+FOREIGN KEY (course) REFERENCES Course(code),
+FOREIGN KEY (program) REFERENCES Program(name),
+PRIMARY KEY (course,program)
+);
+
+CREATE TABLE MandatoryBranch(
+course TEXT,
+branch TEXT,
+program TEXT,
+FOREIGN KEY (course) REFERENCES Course(code),
+FOREIGN KEY (branch,program) REFERENCES Branch(name,program),
+PRIMARY KEY (course, branch, program)
+);
+
+CREATE TABLE RecommendedBranch(
+course TEXT,
+branch TEXT,
+program TEXT,
+FOREIGN KEY (course) REFERENCES Course(code),
+FOREIGN KEY (branch,program) REFERENCES Branch(name,program),
+PRIMARY KEY (course, branch, program)
+);
+
+
