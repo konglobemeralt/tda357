31a11bcf1c464fad8b835bb7c7bc01df201522f7
Hugormen <hugo.ekelund@telia.com>
2018-01-24 22:35:47 +0100

    Add Hosts and Student

--- tables.sql
+++ tables.sql
@@ -1,15 +1,27 @@
 -- noinspection SqlDialectInspectionForFile
 
-CREATE TABLE program (
+CREATE TABLE Program (
   name TEXT PRIMARY KEY,
   abbreviation TEXT
 );
 
-CREATE TABLE department (
+CREATE TABLE Department (
   name TEXT PRIMARY KEY,
   abbreviation TEXT UNIQUE
 );
 
-CREATE TABLE hosts (
-department TEXT PRIMARY KEY
-)
+CREATE TABLE Hosts (
+department TEXT,
+program TEXT,
+FOREIGN KEY (department) REFERENCES Department(name),
+FOREIGN KEY (program) REFERENCES Program(name),
+PRIMARY KEY (department, program)
+);
+
+CREATE TABLE Student(
+ssn TEXT PRIMARY KEY,
+program TEXT,
+login TEXT UNIQUE,
+FOREIGN KEY (program) REFERENCES Program(name),
+UNIQUE (ssn,program)
+);
