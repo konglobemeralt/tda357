7af430557ec802095cf21806b5e2be613da152d9
Hugormen <hugo.ekelund@telia.com>
2018-01-25 22:42:36 +0100

    add insert to git

---
+++ insert.sql
@@ -0,0 +1,27 @@
+INSERT INTO Program (name,abbreviation) VALUES
+  ('Informationsteknik','TKITE'),
+  ('Maskinteknik','TKMAS'),
+  ('Kemiteknik', 'TKKMT'),
+  ('Teknisk Matematik', 'TKTEM'),
+  ('Teknisk Fysik', 'TKTFY');
+
+INSERT INTO Student VALUES
+  ('199607097733','Informationsteknik','login'),
+  ('199508023322', 'Maskinteknik', 'hund'),
+  ('199703022182', 'Kemiteknik', 'password123'),
+  ('19960201', 'Informationsteknik','katt');
+
+INSERT INTO Department VALUES
+  ('DATA- OCH INFORMATIONSTEKNIK', 'TDA'),
+  ('ARKITEKTUR OCH SAMHÄLLSBYGGNADSTEKNIK', 'ACE'),
+  ('FYSIK', 'F'),
+  ('MATEMATISKA VETENSKAPER', 'MVE');
+
+
+
+INSERT INTO Course VALUES
+  ('MVE045','MATEMATISK ANALYS','7.5','MATEMATISKA VETENSKAPER'),
+  ('TDA545','OBJEKTORIENTERAD PROGRAMMERING', '7.5', 'DATA- OCH INFORMATIONSTEKNIK'),
+  ('MVE051','DISKRET MATEMATIK OCH STATISTIK','7.5','MATEMATISKA VETENSKAPER'),
+  ('TDA551','GRUNDLÄGGANDE PROGRAMMERING', '7.5', 'DATA- OCH INFORMATIONSTEKNIK');
+
