66ed75cbc4d967e9f6a137e07ee9edcfdaf7de7e
Hugormen <hugo.ekelund@telia.com>
2018-01-25 16:06:56 +0100

    fix not null check

--- tables.sql
+++ tables.sql
@@ -1,104 +1,104 @@
 -- noinspection SqlDialectInspectionForFile
 
 CREATE TABLE Program (
-  name TEXT PRIMARY KEY,
-  abbreviation TEXT
+  name TEXT NOT NULL PRIMARY KEY,
+  abbreviation TEXT NOT NULL
 );
 
 CREATE TABLE Department (
-  name TEXT PRIMARY KEY,
-  abbreviation TEXT UNIQUE
+  name TEXT NOT NULL PRIMARY KEY,
+  abbreviation TEXT NOT NULL UNIQUE
 );
 
 CREATE TABLE Hosts (
-department TEXT,
-program TEXT,
+department TEXT NOT NULL,
+program TEXT NOT NULL,
 FOREIGN KEY (department) REFERENCES Department(name),
 FOREIGN KEY (program) REFERENCES Program(name),
 PRIMARY KEY (department, program)
 );
 
 CREATE TABLE Student(
-ssn TEXT PRIMARY KEY,
-program TEXT,
-login TEXT UNIQUE,
+ssn TEXT NOT NULL PRIMARY KEY,
+program TEXT NOT NULL,
+login TEXT NOT NULL UNIQUE,
 FOREIGN KEY (program) REFERENCES Program(name),
 UNIQUE (ssn,program)
 );
 
 CREATE TABLE Branch(
-name TEXT,
-program TEXT,
+name TEXT NOT NULL,
+program TEXT NOT NULL,
 FOREIGN KEY (program) REFERENCES Program(name),
 PRIMARY KEY(name,program)
 );
 
 CREATE TABLE BelongsTo(
-student TEXT,
-branch TEXT,
-program TEXT,
+student TEXT NOT NULL,
+branch TEXT NOT NULL,
+program TEXT NOT NULL,
 FOREIGN KEY (student) REFERENCES Student(ssn),
 FOREIGN KEY (student,program) REFERENCES Student(ssn,program),
 FOREIGN KEY (branch,program) REFERENCES Branch(name, program)
 );
 
 CREATE TABLE Course(
-code TEXT PRIMARY KEY,
-name TEXT,
-credits TEXT,
-department TEXT,
+code TEXT NOT NULL PRIMARY KEY,
+name TEXT NOT NULL,
+credits TEXT NOT NULL,
+department TEXT NOT NULL,
 FOREIGN KEY (department) REFERENCES Department(name)
 );
 
 CREATE TABLE Prerequisite(
-course TEXT,
-prerequisite TEXT,
+course TEXT NOT NULL,
+prerequisite TEXT NOT NULL,
 FOREIGN KEY (course) REFERENCES Course(code),
 FOREIGN KEY (prerequisite) REFERENCES Course(code),
 PRIMARY KEY (course,prerequisite)
 );
 
 CREATE TABLE Classification(
-name TEXT PRIMARY KEY
+name TEXT NOT NULL PRIMARY KEY
 );
 
 CREATE TABLE Classified(
-course TEXT,
-classification TEXT,
+course TEXT NOT NULL,
+classification TEXT NOT NULL,
 FOREIGN KEY (course) REFERENCES Course(code),
 FOREIGN KEY (classification) REFERENCES Classification(name),
 PRIMARY KEY (classification, course)
 );
 
 CREATE TABLE MandatoryProgram(
-course TEXT,
-program TEXT,
+course TEXT NOT NULL,
+program TEXT NOT NULL,
 FOREIGN KEY (course) REFERENCES Course(code),
 FOREIGN KEY (program) REFERENCES Program(name),
 PRIMARY KEY (course,program)
 );
 
 CREATE TABLE MandatoryBranch(
-course TEXT,
-branch TEXT,
-program TEXT,
+course TEXT NOT NULL,
+branch TEXT NOT NULL,
+program TEXT NOT NULL,
 FOREIGN KEY (course) REFERENCES Course(code),
 FOREIGN KEY (branch,program) REFERENCES Branch(name,program),
 PRIMARY KEY (course, branch, program)
 );
 
 CREATE TABLE RecommendedBranch(
-course TEXT,
-branch TEXT,
-program TEXT,
+course TEXT NOT NULL,
+branch TEXT NOT NULL,
+program TEXT NOT NULL,
 FOREIGN KEY (course) REFERENCES Course(code),
 FOREIGN KEY (branch,program) REFERENCES Branch(name,program),
 PRIMARY KEY (course, branch, program)
 );
 
 CREATE TABLE Registered(
-  student TEXT,
-  course TEXT,
+  student TEXT NOT NULL,
+  course TEXT NOT NULL,
   FOREIGN KEY (student) REFERENCES Student(ssn),
   FOREIGN KEY (course) REFERENCES Course(code),
   PRIMARY KEY (student,course)
@@ -107,26 +107,26 @@ CREATE TABLE Registered(
 CREATE TYPE GRADE AS ENUM ('U','3','4','5');
 
 CREATE TABLE Taken(
-  student TEXT,
-  course TEXT,
-  grade GRADE,
+  student TEXT NOT NULL,
+  course TEXT NOT NULL,
+  grade GRADE NOT NULL,
   FOREIGN KEY (student) REFERENCES Student(ssn),
   FOREIGN KEY (course) REFERENCES Course(code),
   PRIMARY KEY ( student,course)
 );
 
 CREATE TABLE LimitedCourse(
-  code TEXT,
-  seats NUMERIC CHECK (seats >= 0),
+  code TEXT NOT NULL,
+  seats NUMERIC NOT NULL CHECK (seats >= 0),
   FOREIGN KEY (code) REFERENCES Course(code),
   PRIMARY KEY (code)
 
 );
 
 CREATE TABLE WaitingList(
-  student TEXT,
-  course TEXT,
-  position NUMERIC CHECK (position > 0),
+  student TEXT NOT NULL,
+  course TEXT NOT NULL,
+  position NUMERIC NOT NULL CHECK (position > 0),
   FOREIGN KEY (student) REFERENCES Student(ssn),
   FOREIGN KEY (course) REFERENCES LimitedCourse(code),
   UNIQUE (position,course),
